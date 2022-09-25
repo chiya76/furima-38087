448a5e0f935ffa77228c2a010cea6ea868f54f8e
chiya76 <cgmn13837@gmail.com>
2022-09-26 00:56:38 +0900

    アソシエーション、バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_one_attached :image
   
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -9,9 +9,9 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days_to_ship
 
-  validates :item_name, :string, presence: true, length: { maximum: 40 }
-  validates :description, :text, presence: true, length: { maximum: 1000 }
-  validates :price, :integer, presence: true, numericality: { in: 300..9999999 }
+  validates :item_name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :price, presence: true, numericality: { in: 300..9999999 }
   
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :status_id, numericality: { other_than: 1, message: "can't be blank"}
