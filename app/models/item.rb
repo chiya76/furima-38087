4d372c9445e58fa5aadb06bb6bc74ab8d8d16ac2
chiya76 <cgmn13837@gmail.com>
2022-09-26 23:12:09 +0900

    エラーメッセージ・バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,15 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days_to_ship
 
+  validates :image, presence: true
   validates :item_name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
-  validates :price, presence: true, numericality: { in: 300..9999999 }
   
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :status_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank"}
+
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "must be between 300 and 9999999" },format: { with: /\A[0-9]+\z/, message: "must be half-width numbers" }
 end
