6246d7dce22ad49199684675c3705d41b5a0c6a8
chiya76 <cgmn13837@gmail.com>
2022-10-02 16:49:47 +0900

    rubocop実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,5 +20,5 @@ class Item < ApplicationRecord
   validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" }
 
   validates :price, presence: true, format: { with: /\A[0-9]+\z/ },
-                    numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'must be half-width integer between 300 and 9999999' }
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'must be half-width integer between 300 and 9999999' }
 end
