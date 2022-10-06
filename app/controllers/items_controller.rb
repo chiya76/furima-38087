74e49d4e6b00c97eb8a5866c464f8a5eb2a35cbd
chiya76 <cgmn13837@gmail.com>
2022-10-06 23:31:18 +0900

    コントローラーの修正
    ・edit設定
    ・update設定
    ・自分が投稿していない商品の編集ページへはアクセスできない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.order('created_at DESC')
@@ -23,10 +24,31 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.new(item_params)
+    item = Item.find(params[:id])
+    item.update(item_params)
+
+    if item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :description, :price, :category_id, :status_id, :delivery_charge_id, :prefecture_id,
                                  :days_to_ship_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
   end
 end
