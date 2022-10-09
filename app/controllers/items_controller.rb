932d9aaa25ba9e0f86307b344dc7bc76a5f1eef8
chiya76 <cgmn13837@gmail.com>
2022-10-09 20:08:04 +0900

    コントローラー修正(set_itemの記述)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: :edit
 
   def index
@@ -21,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
 
     if @item.save
@@ -46,8 +44,11 @@ class ItemsController < ApplicationController
                                  :days_to_ship_id, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
+  def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
     redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
   end
 end
