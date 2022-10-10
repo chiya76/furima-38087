99e0166e4915fd202f43fb338447b45bdfd9c8b5
chiya76 <cgmn13837@gmail.com>
2022-10-10 22:41:21 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, only: :edit
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -30,6 +30,16 @@ class ItemsController < ApplicationController
   def update
     if @item.update(item_params)
       redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+
+    if @item.destroy
+      redirect_to root_path(@item)
     else
       render :edit
     end
