9203b0eed811e02aeb81c4f17d04e643194eeb6e
chiya76 <cgmn13837@gmail.com>
2022-10-19 02:12:24 +0900

    クレジットカード決済機能の実装
    order_addressでカード情報を取得する(java)
    tokenの設定
    webpacker導入
    環境変数の設定
    バリデーションの追加
    テストコードの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,6 +20,15 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:post_code, :prefecture, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:post_code, :prefecture, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,8 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture, :municipality, :address, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :post_code, :prefecture, :municipality, :address, :building_name, :phone_number, :token
 
   with_options presence: true do
+    validates :token
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :user_id, :item_id, :municipality, :address
     validates :phone_number, format: {with: /\d{10,11}/, message: "is invalid"}, length: {maximum: 11, message: "is invalid"}
